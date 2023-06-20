908d1968bd872d2e0323b0735384015a37ed550b
haisehi <88788629+haisehi@users.noreply.github.com>
2023-06-20 12:27:13 +0700

    update 20/06/2023

---
+++ AppBanNuocHoa/app/src/main/java/com/example/appbannuochoa/utils/Utils.java
@@ -0,0 +1,15 @@
+package com.example.appbannuochoa.utils;
+
+import com.example.appbannuochoa.model.GioHang;
+import com.example.appbannuochoa.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Utils {
+    public static final String BASE_URL="http://172.20.10.2/bannuochoa/";
+    public static List<GioHang> manggiohang;
+    public static List<GioHang> mangmuahang = new ArrayList<>();
+    public static User user_current = new User();
+
+}
